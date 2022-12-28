8921aec2aa4b18e9efecfd4a16cc4dd06e1223a2
Roben Castagna Lunardi <roben.lunardi@restinga.ifrs.edu.br>
2022-12-28 19:56:27 -0300

    updating automated bib

--- publications/bib2yaml.py
+++ publications/bib2yaml.py
@@ -12,8 +12,25 @@ with open(str_input, 'r') as fr:
  
 # list the output line
 list_output = []
-stringDemaisCampos =''
- 
+
+# string in publist.yml
+stringTitle = ''
+stringAuthors = ''
+stringUrl = ''
+stringImage ='' 
+stringDisplay =''
+stringYear =''
+stringDoi = ''
+stringCode= ''
+stringAbstract=''
+# each string correspond to one line in publist.yml, complete (except title):
+stringComplete =''
+booktitle = '' # (confname/journalname)
+volume = ''
+issue= '' 
+pages = ''
+publisher = ''
+
 # go through the lines
 for str_line in list_lines:
      
@@ -21,15 +38,51 @@ for str_line in list_lines:
     if str_line.startswith('@'):
         # sg_t1 = re.search('^@(.*){(.*),', str_line)
         sg_t1 = re.search('^@(.*){(.*),', str_line)
-        print (sg_t1)
-        print ("passei aqui ")
         str_id = re.sub(':','',sg_t1.group(2))
         str_first = '\n-%s: &%s' % (sg_t1.group(1), str_id)
         # list_output.append(str_first)
         # list_output.append(list_output_demaiscampos)
-        if(stringDemaisCampos != ''):
-            list_output.append(stringDemaisCampos+'\n')
-            stringDemaisCampos = ''
+        if(stringComplete != ''):
+            # mount stringDisplay with the fields
+            if booktitle !='':
+                stringDisplay = booktitle
+                booktitle=''
+            if volume != '':
+                if issue != '':
+                    stringDisplay = stringDisplay + ', '+volume +'(' + issue + '), '
+                    volume =''
+                    issue = ''
+                else:
+                    stringDisplay = stringDisplay + ', '+ volume
+                    volume =''
+            if pages != '':
+                stringDisplay = stringDisplay+ ', '+pages
+                pages = ''
+            if publisher != '': 
+                stringDisplay = stringDisplay+ '. '+publisher                 
+            
+            # mount complete string
+            stringComplete = stringComplete+stringTitle+'\n'+stringAuthors
+            stringTitle = ''
+            stringAuthors = ''
+            if stringUrl != '': 
+                stringComplete = stringComplete+'\n'+stringUrl
+                stringUrl = ''
+            if stringDisplay != '': 
+                stringDisplay = '  display: "'+stringDisplay+'".'
+                stringComplete = stringComplete+'\n'+stringDisplay
+                stringDisplay = ''
+            if stringYear != '': 
+                stringComplete = stringComplete+'\n'+stringYear
+                stringYear = '' 
+            if stringDoi != '': 
+                stringComplete = stringComplete+'\n'+stringDoi
+                stringDoi = ''    
+            if stringAbstract != '': 
+                stringComplete = stringComplete+'\n'+stringAbstract
+                stringAbstract = ''
+            list_output.append(stringComplete+'\n')
+            stringComplete = ''
         # list_output_demaiscampos = []
         print(str_first)
  
@@ -39,7 +92,6 @@ for str_line in list_lines:
         print ("dentro do elsif")
         # sg_tn = re.search('^\t(.*) = {(.*?)}', str_line)
         sg_tn = re.search('^  (.*) = {(.*?)}', str_line)
-        print ("dentro do elsif")
         print(str_line)
         str_cat = sg_tn.group(1).lower()
         str_val = sg_tn.group(2)
@@ -50,8 +102,11 @@ for str_line in list_lines:
  
         # make a list of all the authors
         if str_cat== 'title':
-            str_gen_out = '- %s: "%s"' % (str_cat, str_val)
-            list_output.append(str_gen_out)
+            # str_gen_out = '- %s: "%s"' % (str_cat, str_val)
+            str_gen_out = '%s: "%s"' % (str_cat, str_val)
+            stringComplete = '- '
+            stringTitle = str_gen_out
+            # list_output.append(str_gen_out)
 
         elif str_cat=='author':
             list_authors = str_val.split(' and ')
@@ -62,30 +117,101 @@ for str_line in list_lines:
             print(str_auths)
             print("antes do append")
             # list_output.append(str_aut_out)
-            if(stringDemaisCampos==''):
-                stringDemaisCampos=str_aut_out
-            else:
-                stringDemaisCampos=stringDemaisCampos+'\n'+str_aut_out 
+            stringAuthors=str_aut_out
+      
+        elif str_cat=='doi':
+            str_gen_out = '  %s: "%s"' % (str_cat, str_val)
+            # list_output.append(str_gen_out)
+            stringDoi=str_gen_out
 
+        elif str_cat=='url':
+            str_gen_out = '  %s: "%s"' % (str_cat, str_val)
+            # list_output.append(str_gen_out)
+            stringUrl=str_gen_out
+       
+        elif str_cat=='abstract':
+            str_gen_out = '  %s: "%s"' % (str_cat, str_val)
+            # list_output.append(str_gen_out)
+            stringAbstract=str_gen_out
+            
+        elif str_cat == 'year':
+            str_jt_out = '  %s: %s' % (str_cat, str_val)
+            # list_output.append(str_jt_out)
+            stringYear=str_jt_out
+
+        #display: booktitle, volume(issue), pages. publisher.  
+        elif str_cat == 'booktitle':
+            booktitle = '%s' % (str_val)
+        elif str_cat == 'volume':
+            volume = '%s' % (str_val)
+        elif str_cat == 'issue':
+            issue = '%s' % (str_val)
+        elif str_cat == 'pages':
+            pages = '%s' % (str_val)
+        elif str_cat == 'publisher':
+            publisher = '%s' % (str_val)            
         else:
-            # all the integer values
-            list_ints = ['number','volume','read','year']
-            if str_cat in list_ints:
-                str_jt_out = '  %s: %s' % (str_cat, str_val)
-                # list_output.append(str_jt_out)
-                if(stringDemaisCampos==''):
-                    stringDemaisCampos=str_jt_out
-                else:
-                    stringDemaisCampos=stringDemaisCampos+'\n'+str_jt_out 
+            print('not mapped category ' + str_cat)
+            
+        # else:
+        #     # all the integer values
+        #     list_ints = ['number','volume','read','year']
+        #     if str_cat in list_ints:
+        #         str_jt_out = '  %s: %s' % (str_cat, str_val)
+        #         # list_output.append(str_jt_out)
+        #         if(stringComplete==''):
+        #             stringComplete=str_jt_out
+        #         else:
+        #             stringComplete=stringComplete+'\n'+str_jt_out 
                  
-            # all the string values
-            else:
-                str_gen_out = '  %s: "%s"' % (str_cat, str_val)
-                # list_output.append(str_gen_out)
-                if(stringDemaisCampos==''):
-                    stringDemaisCampos=str_gen_out
-                else:
-                    stringDemaisCampos=stringDemaisCampos+'\n'+str_gen_out 
-list_output.append(stringDemaisCampos) 
+        #     # all the string values
+        #     else:
+        #         str_gen_out = '  %s: "%s"' % (str_cat, str_val)
+        #         # list_output.append(str_gen_out)
+        #         if(stringComplete==''):
+        #             stringComplete=str_gen_out
+        #         else:
+        #             stringComplete=stringComplete+'\n'+str_gen_out 
+
+# for the last reference
+if booktitle !='':
+    stringDisplay = booktitle
+    booktitle=''
+if volume != '':
+    if issue != '':
+        stringDisplay = stringDisplay + ', '+volume +'(' + issue + '), '
+        volume =''
+        issue = ''
+    else:
+        stringDisplay = stringDisplay + ', '+ volume
+        volume =''
+if pages != '':
+    stringDisplay = stringDisplay+ ', '+pages
+    pages = ''
+if publisher != '': 
+    stringDisplay = stringDisplay+ '. '+publisher                    
+
+# mount complete string
+stringComplete = stringComplete+stringTitle+'\n'+stringAuthors
+stringTitle = ''
+stringAuthors = ''
+if stringUrl != '': 
+    stringComplete = stringComplete+'\n'+stringUrl
+    stringUrl = ''
+if stringDisplay != '': 
+    stringDisplay = '  display: "'+stringDisplay+'."'
+    stringComplete = stringComplete+'\n'+stringDisplay
+    stringDisplay = ''
+if stringYear != '': 
+    stringComplete = stringComplete+'\n'+stringYear
+    stringYear = '' 
+if stringDoi != '': 
+    stringComplete = stringComplete+'\n'+stringDoi
+    stringDoi = ''    
+if stringAbstract != '': 
+    stringComplete = stringComplete+'\n'+stringAbstract
+    stringAbstract = ''
+list_output.append(stringComplete+'\n')
+
 with open(str_output,'w') as fw:
     fw.write('\n'.join(list_output))
